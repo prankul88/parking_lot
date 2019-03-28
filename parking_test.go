0c023946c4e3786add25851c153b9d3e57e5d13d
prankul <prankul.mahajan88@gmail.com>
2019-03-28 12:21:20 +0530

    Added additional test cases

--- parking_test.go
+++ parking_test.go
@@ -9,4 +9,35 @@ func TestNewLot(t *testing.T) {
 	if len(parkingLot) != 4 {
 		t.Errorf("Expected capacity to be 4 but got %v", len(parkingLot))
 	}
+
+	addCar("DL-4C-NE-8184", "white")
+	addCar("DL-4C-NE-8185", "white")
+	addCar("DL-4C-NE-8186", "white")
+
+	j := 0
+	for i := range parkingLot {
+		flag := details{carNo: "", carColor: ""}
+		if parkingLot[i] == flag {
+			j++
+		}
+	}
+
+	if j != 1 {
+		t.Errorf("Expected empty spaces to be 1 but got %v", j)
+	}
+
+	leaveCar(2)
+
+	j = 0
+	for i := range parkingLot {
+		flag := details{carNo: "", carColor: ""}
+		if parkingLot[i] == flag {
+			j++
+		}
+	}
+
+	if j != 2 {
+		t.Errorf("Expected empty spaces to be 2 but got %v", j)
+	}
+
 }
